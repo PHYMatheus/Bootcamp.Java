e997c7ea1fea46fdd7726c698dd71d6ee79648f2
PHYMatheus <matheusguilherme00221@gmail.com>
2026-08-13 23:48:18 -0300

    Aula Construtores

--- src/JavaPoo/Exercicios/Anime.java
+++ src/JavaPoo/Exercicios/Anime.java
@@ -7,6 +7,21 @@ public class Anime {
     private String tipo;
     private String genero;
     private String empresa;
+    private int ano;
+
+    // Utilizando um Construtor.
+    public Anime(String nome, int episodios, String tipo, String genero, String empresa) {
+        this.nome = nome;
+        this.episodios = episodios;
+        this.tipo = tipo;
+        this.genero = genero;
+        this.empresa = empresa;
+    }
+    // Sobrecarga de Construtor.
+    public Anime(String nome, int episodios, String tipo, String genero, String empresa, int ano) {
+        this(nome,episodios,tipo,genero,empresa);
+        this.ano = ano;
+    }
 
     public void setNomeAnime(String nome){this.nome = nome;}
     public String getNomeAnime(){return this.nome;}
@@ -18,24 +33,12 @@ public class Anime {
     public void setGenero(String genero){this.genero = genero;}
     public String getEmpresa(){return this.empresa;}
     public void setEmpresa(String empresa){this.empresa = empresa;}
+    public int getAno() {return ano;}
+    public void setAno(int ano) {this.ano = ano;}
 
     public void imprimir(){
         System.out.println("Nome: " + this.nome);
         System.out.println("Episodios: " + this.episodios);
         System.out.println("Tipo: " + this.tipo);
     }
-
-    public void init(String nome, int episodios, String tipo, String genero){
-        this.nome = nome;
-        this.tipo = tipo;
-        this.episodios = episodios;
-        this.genero = genero;
-    }
-    // Aqui utilizando a Sobrecarga de método para adicionar um Atributo no Método.
-    public void init(String nome, int episodios, String tipo, String genero, String empresa){
-        init(nome, episodios, tipo, genero);
-        this.empresa = empresa;
-    }
-
-
 }
---
+++ src/JavaPoo/Exercicios/AnimeTest.java
@@ -0,0 +1,10 @@
+package JavaPoo.Exercicios;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+
+        Anime anime = new Anime("Naruto", 360, 
+                "TV", "Ação", "Nickelodeon", 2027);
+        anime.imprimir();
+    }
+}
